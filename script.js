a152c2696f4f6a195c874408bb99f6bd97052d78
manishloginn <132683386+manishloginn@users.noreply.github.com>
2024-02-07 22:51:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-var dateDiffInDays = function (date1, date2) {
-  //   write your code here
+ 
+let dateDiffInDays = function (dateOne, dateTwo) {
+  // Write your code here
+ 
+  const oneDay = 24 * 60 * 60 * 1000; // Hours in a day * minutes in an hour * seconds in a minute * milliseconds in a second
+  const firstDate = new Date(dateOne);
+  const secondDate = new Date(dateTwo);
+ 
+  const diffInTime = secondDate.getTime() - firstDate.getTime();
+  const diffInDays = Math.round(diffInTime / oneDay);
+  return diffInDays;
 };
-
+ 
 // Do not change the code below.
+ 
 const dateOne = prompt("Enter Start Date.");
 const dateTwo = prompt("Enter End Date.");
 alert(dateDiffInDays(dateOne, dateTwo));
